f4657b0c2d919a80b1c4103042cfa2912054af0a
JayeshST <thombarejayesh001@gmail.com>
2023-11-03 22:53:14 +0530

    FIRST COMMIT

---
+++ src/tester/TestDeSerilization.java
@@ -0,0 +1,26 @@
+package tester;
+
+import java.util.Map;
+import static utils.IO2Utils.restoreProductDetails;
+import java.util.Scanner;
+
+import com.shop.core.Product;
+
+//import static utils.IO2Utils.restoreProductDetails;
+
+public class TestDeSerilization {
+
+	public static void main(String[] args) {
+		// restore product details
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter file name");
+			Map<Integer,Product> productDetails = restoreProductDetails(sc.nextLine());
+			System.out.println("All products : ");
+			productDetails.forEach((k, v) -> System.out.println(v));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
